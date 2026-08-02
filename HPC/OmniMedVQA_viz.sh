1aaf6641f39267badcf6a5b5f7ada56159c8be14
TanukiDong <eurondelta@gmail.com>
2026-08-02 18:33:46 +0100

    rename/move files

--- HPC/OmniMedVQA_viz.sh
+++ HPC/OmniMedVQA_viz.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=omni_viz
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=16G
-#SBATCH --output=./output/omni_viz_output.txt
-#SBATCH --error=./output/omni_viz_error.txt
+#SBATCH --output=./output/OmniMedVQA_viz_output.txt
+#SBATCH --error=./output/OmniMedVQA_viz_error.txt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=twanavit1@sheffield.ac.uk
 
@@ -12,4 +12,4 @@ module load Anaconda3/2024.02-1
 
 source activate myspark
 
-spark-submit --driver-memory 32g ./omni_viz.py
+spark-submit --driver-memory 32g ./OmniMedVQA_viz.py
